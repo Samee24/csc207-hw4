768c858b2c08d7e511d3367731f5d2ff78df8da2
Samee24 <sameescouser24@gmail.com>
2014-09-18 21:51:11 -0500

    Update StringUtils.java

--- src/edu/grinnell/zahidmuh17/csc207/assignment4/StringUtils.java
+++ src/edu/grinnell/zahidmuh17/csc207/assignment4/StringUtils.java
@@ -32,20 +32,20 @@ public class StringUtils {
 				arrayList.add(stringWithSomeChar.substring(tracker, index));
 				//Update tracker, since substrings are inclusive of the bottom index
 				tracker = index + 1;
-			}
+			}//if
 			
 			//Special case for our last substring
 			if (index == stringWithSomeChar.length() - 1){
 				arrayList.add(stringWithSomeChar.substring(tracker));
 				
-			}
+			}//if
 				
-		}
+		}//for
 		
 		//Transform to a String[]
 		return transferFromArrayList(arrayList);
 		
-	}
+	}//splitAt(String stringWithSomeChar, char someChar)
 
 	public static String[] splitCSV(String input) {
 
@@ -65,7 +65,7 @@ public class StringUtils {
 			if ((currentChar == ',') && !inQuote) {
 				arrayList.add(buffer.toString());
 				buffer = new StringBuffer("");
-			}
+			}//if
 			// Special case: Last element
 			else if (index == input.length() - 1) {
 
@@ -73,27 +73,30 @@ public class StringUtils {
 				arrayList.add(buffer.toString());
 				buffer = new StringBuffer("");
 
-			}// Special case for two consecutive double quotes
+			}//else if
+			// Special case for two consecutive double quotes
 			else if (currentChar == '\"' && input.charAt(index + 1) == '\"') {
 
 				index++;
 				buffer.append(currentChar);
 
-			} // Case when there's a single quote, indicating we're either
+			}//else if
+			// Case when there's a single quote, indicating we're either
 				// starting or ending a quote
 			else if (currentChar == '\"') {
 				// Toggle the boolean
 				inQuote = (inQuote) ? false : true;
 
-			} else {
+			}//else if
+			else {
 				buffer.append(currentChar);
 
-			}
+			}//else
 
-		}
+		}//for
 
 		return transferFromArrayList(arrayList);
-	}
+	}//splitCSV(String input)
 
 	// Helper method to transfer data from an arrayList to a String[]
 	public static String[] transferFromArrayList(ArrayList<String> stringArray) {
@@ -104,10 +107,10 @@ public class StringUtils {
 
 			result[indexArr] = stringArray.get(indexArr);
 
-		}
+		}//for
 
 		return result;
 
-	}
+	}//transferFromArrayList(ArrayList<String> stringArray)
 
 }
